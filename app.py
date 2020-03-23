fca789aa4f1f433ad1d053dfef1a4adf27317421
Avery Quinn <avery.quinn1493@gmail.com>
2020-03-23 16:51:00 -0400

    create hello and about page

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 
 # app.py
 
+# import Flask object
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
+# initilize flask app 
+app = Flask(__name__) # app name of file
+
+
+# decorator runs function
+@app.route('/') # home page
 def hello_world():
+    print("Visit Hello Page")
     return 'Hello, World!'
+
+# 
+@app.route('/about') # about page
+def about():
+    print("Visit About Page")
+    return 'About Me'
