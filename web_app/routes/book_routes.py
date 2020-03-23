66bb27fcf1f6e1178408bb7eaea31e4db9cb83c1
Avery Quinn <avery.quinn1493@gmail.com>
2020-03-23 18:30:05 -0400

    Render books page HTML

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/book_routes.py
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 # import from init file
 book_routes = Blueprint("book_routes", __name__)
@@ -8,6 +8,17 @@ book_routes = Blueprint("book_routes", __name__)
 
 # attach route to bluprint object
 @book_routes.route('/books.json') # home page
+def list_books():
+    print("Request Books JSON Page")
+    # fake database
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"},
+    ]
+    return jsonify(books)
+
+@book_routes.route('/books') # home page
 def books():
     print("Visit Book Page")
     # fake database
@@ -16,4 +27,5 @@ def books():
         {"id": 2, "title": "Book 2"},
         {"id": 3, "title": "Book 3"},
     ]
-    return jsonify(books)
+    return render_template("books.html") #return html file located in templates
+
