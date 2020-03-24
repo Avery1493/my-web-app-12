da3efdb414b03e6aadd8aafdb810877f404a739f
Avery Quinn <avery.quinn1493@gmail.com>
2020-03-24 12:58:44 -0400

    Add Basilica service

---
+++ web_app/services/basilica_service.py
@@ -0,0 +1,26 @@
+
+# python web_app/services/basilica_service.py
+
+import basilica
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("BASCILICA_API_KEY")
+
+connection = basilica.Connection(API_KEY)
+print(type(connection))
+print("----------")
+
+
+sentence = "Hello again!"
+print(sentence)
+embeddings = connection.embed_sentence(sentence)
+print("----------")
+
+
+sentences = ["Hello world!", "How are you?"]
+print(sentences)
+embeddings = connection.embed_sentences(sentences)
+print(list(embeddings)) # [[0.8556405305862427, ...], ...]
